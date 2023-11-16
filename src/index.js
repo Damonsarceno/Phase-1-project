6bbf0d6888793cbcc89879f7e364af2c99ea1368
Damon sarceno <Damonsarceno@gmail.com>
2023-11-16 18:41:52 -0500

    added some css and filled render function

--- src/index.js
+++ src/index.js
@@ -1,38 +1,45 @@
 
+  
+
 fetch("http://localhost:3000/City's")
-  .then(res => res.json())
-  .then(citys => {
-    citys.forEach((city => renderCitys(city)));
-  })
+.then(res => res.json())
+.then(citys => {
+  citys.forEach((city => renderCitys(city)));
+
+})
 
 
 
 
 
-
-
-  citys.forEach((city) => {
+function renderCitys (city) {
 
     const li = document.createElement('li');
-    //li.className = 'city-list';
+    li.className = 'list-li';
     
    
     const h3 = document.createElement('h3');
     h3.textContent = city.location;
-    li.append(h3);
+   
 
    
-    const temp = document.createElement('p');
-    p.textContent = city.Temperature-F
-    li.append(temp);
+    const pTemp = document.createElement('p');
+    pTemp.textContent = city.TemperatureF
+   
 
-    const temp2 = document.createElement('p');
-    p.textContent = city.Temperature-C
-    li.append(temp2);
+    const pTempC = document.createElement('p');
+    pTempC.textContent = city.TemperatureC
+    
+
+    const pWind = document.createElement('p');
+    pWind.textContent = city.WindSpeedMph
+
+
+    const pWindK = document.createElement('p');
+    pWindK.textContent = city.WindSpeedKmh
 
 
 
-
+  li.append(h3, pTemp, pTempC, pWind, pWindK);
   document.getElementById('city-list').append(li);
-})
-//renderCitys();
+}
